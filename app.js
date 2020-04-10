85c0f0071bcb1820068295c017ab44c50fd44cb6
Alberto Pintado <63448359+pintadolozano@users.noreply.github.com>
2020-04-10 11:43:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ let fahrenheit = celsius * (9/5) + 32;
 /* Convertida la tempertura a Fahrenheit con la
 posibilidad de variar más adelante el valor */
 fahrenheit = Math.floor(fahrenheit);
+// Redondeado y convertido en entero
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+// Output de la temperatura en ºF
 let newton = celsius * (33/100);
+// Declarada la conversión a grados Newton
 newton = Math.floor(newton);
+// Redondeado y convertido en entero
 console.log(`The temperature is ${newton} degrees Newton`);
+// Output de la temperatura en ºN
